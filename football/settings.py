7d75f98d48787787b29d0619ddbe4102dd73cf11
Milos Stankovic <tomibre397@.com>
2025-09-29 13:34:09 +0200

    first commit

--- football/settings.py
+++ football/settings.py
@@ -89,10 +89,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'football_db',
-        'USER': 'milos',
-        'PASSWORD': 'ubuntu',
+        'USER': 'postgres',
+        'PASSWORD': 'milos',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
